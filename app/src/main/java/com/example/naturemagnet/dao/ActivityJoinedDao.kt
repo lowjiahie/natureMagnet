25306e989d6829c6f779d156d21e6e101e5472ac
ngjc-wm19@student.tarc.edu.my <ngjc-wm19@student.tarc.edu.my>
2022-10-02 17:25:21 +0800

    added query for selecting activityJoined

--- app/src/main/java/com/example/naturemagnet/dao/ActivityJoinedDao.kt
+++ app/src/main/java/com/example/naturemagnet/dao/ActivityJoinedDao.kt
@@ -2,10 +2,18 @@ package com.example.naturemagnet.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+import com.example.naturemagnet.entity.Activity
 import com.example.naturemagnet.entity.ActivityJoined
 
 @Dao
 interface ActivityJoinedDao {
+    @Query("SELECT * FROM ActivityJoined")
+    fun getAll(): List<ActivityJoined>
+
+    @Query("SELECT * FROM ActivityJoined WHERE ActivityJoined.activityID == :activityId")
+    fun getActivityJoined(activityId: Long): ActivityJoined
+
     @Insert
     fun insertActivityJoined(activityJoined: ActivityJoined)
 
